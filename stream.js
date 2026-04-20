508fb0f844ccc528b58ab71580650e489127aecc
snegupta80-ctrl <snegupta80@gmail.com>
2026-04-20 14:57:34 +0530

    nodejs

--- stream.js
+++ stream.js
@@ -1,8 +1,14 @@
 const fs = require("fs");
 
-const readableStream = fs.createReadStream("hugefile.txt",'utf-8');
-const writeableStream = fs.createWriteStream("hugefile1.txt",'utf-8');
+const readableStream = fs.createReadStream("hugefile.txt", 'utf-8');
+console.log(readableStream)
+const writeableStream = fs.createWriteStream("hugefile1.txt", 'utf-8');
 readableStream.on("data", (buffer) => {
-    console.log(buffer);
-    writeableStream.write(buffer);
+    writeableStream.write('buffer')
+})
+readableStream.on("error", (err) => {
+    console.log(err.message);
+})
+writeableStream.on("error", (err) => {
+    console.log(err.message);
 })
