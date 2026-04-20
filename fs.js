a4740af267b4e282db811784b3941e985a5c7d36
snegupta80-ctrl <snegupta80@gmail.com>
2026-04-20 12:55:29 +0530

    nodejs

--- fs.js
+++ fs.js
@@ -1,31 +1,31 @@
-// const fs = require("fs");
-// // fs.mkdir("docs", (err) => {
-// //     if (err) {
-// //         console.log(err);
-// //     }
-// //     console.log("directory created");
+// // const fs = require("fs");
+// // // fs.mkdir("docs", (err) => {
+// // //     if (err) {
+// // //         console.log(err);
+// // //     }
+// // //     console.log("directory created");
 
 
-// // })
+// // // })
 
 
-// // fs.writeFile("docs/file.txt", "hello world", (err) => {
-// //     if (err) {
-// //         console.log(err);
-// //     }
-// //     console.log("file written")
-// // })
+// // // fs.writeFile("docs/file.txt", "hello world", (err) => {
+// // //     if (err) {
+// // //         console.log(err);
+// // //     }
+// // //     console.log("file written")
+// // // })
 
-// if (fs.existsSync("docs/file.txt")) {
-//     fs.readFile("docs/file.txt", (err, data) => {
-//         if (err) {
-//             console.log(err, message);
-//         }
-//         else {
-//             console.log(data.toString());
-//         }
-//     })
-// }
+// // if (fs.existsSync("docs/file.txt")) {
+// //     fs.readFile("docs/file.txt", (err, data) => {
+// //         if (err) {
+// //             console.log(err, message);
+// //         }
+// //         else {
+// //             console.log(data.toString());
+// //         }
+// //     })
+// // }
 const fs = require("fs");
 
 fs.rm("docs", { recursive: true, force: true }, (err) => {
@@ -35,3 +35,13 @@ fs.rm("docs", { recursive: true, force: true }, (err) => {
         console.log("Directory removed successfully");
     }
 });
+
+// const fs = require("fs");
+
+// fs.unlink("docs/file.txt", (err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("File deleted successfully");
+//     }
+// });
